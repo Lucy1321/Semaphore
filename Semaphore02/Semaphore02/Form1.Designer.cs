d171d34d3a96d017de893c3a1216d604d027aaf2
Lucia Amador <luciaap03@gmail.com>
2020-12-20 22:45:18 -0400

    Add files via upload

---
+++ Semaphore02/Semaphore02/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Semaphore02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.LbContador = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Semaphore02.Properties.Resources.smartphone;
+            this.pictureBox2.Location = new System.Drawing.Point(523, 40);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(273, 356);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Semaphore02.Properties.Resources.semaforo_apagado;
+            this.pictureBox1.Location = new System.Drawing.Point(78, 82);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(198, 291);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(600, 159);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(123, 53);
+            this.btnIniciar.TabIndex = 2;
+            this.btnIniciar.Text = "Cruce Peatonal";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.BtnIniciar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(600, 229);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(123, 53);
+            this.btnCancelar.TabIndex = 3;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.BtnCancelar_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(134, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Semaforo";
+            // 
+            // LbContador
+            // 
+            this.LbContador.AutoSize = true;
+            this.LbContador.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LbContador.Location = new System.Drawing.Point(369, 139);
+            this.LbContador.Name = "LbContador";
+            this.LbContador.Size = new System.Drawing.Size(69, 73);
+            this.LbContador.TabIndex = 5;
+            this.LbContador.Text = "0";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(596, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Peatonal";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LbContador);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LbContador;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Semaphore02/Semaphore02/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Semaphore02
+{
+    public partial class Form1 : Form
+    {
+        int situation = 0;
+        int contador = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            
+            switch (situation)
+            {
+                case 0:
+                    timer2.Start();
+                    pictureBox1.Image = Semaphore02.Properties.Resources.semaforo_verde;
+                    btnCancelar.Enabled = true;
+                    label2.Text = "Favor Cruzar";
+                    timer1.Interval = 3000;
+                    contador = 11;
+                    contador = contador - 1;
+                    this.LbContador.Text = Convert.ToInt32(contador).ToString();
+                    if (contador == 0)
+                    {
+                        this.timer2.Enabled = false;
+                    }
+                    situation = 1;
+                    break;
+                case 1:
+                    pictureBox1.Image = Semaphore02.Properties.Resources.semaforo_verde;
+                    btnCancelar.Enabled = false;
+                    timer1.Interval = 7000;
+                    situation = 2;
+                    break;
+
+                case 2:
+                    timer2.Start();
+                    pictureBox1.Image = Semaphore02.Properties.Resources.semaforo_amarillo2;
+                    timer1.Interval = 4000;
+                    label2.Text = "Favor Cruzar";
+                    //label2.Enabled = false;
+                    btnCancelar.Enabled = false;
+                    contador = 5;
+                    contador = contador - 1;
+                    this.LbContador.Text = Convert.ToInt32(contador).ToString();
+                    if (contador == 0)
+                    {
+                        this.timer2.Enabled = false;
+                    }
+                    situation = 3;
+                    break;
+
+                case 3:
+                    timer2.Start();
+                    pictureBox1.Image = Semaphore02.Properties.Resources.semaforo_rojo;
+                    //label2.Text = "No Cruzar";
+                    label2.Enabled = false;
+                    timer1.Interval = 5000;
+                    btnCancelar.Enabled = false;
+                    contador = 6;
+                    contador = contador - 1;
+                    this.LbContador.Text = Convert.ToInt32(contador).ToString();
+                    if (contador == 0)
+                    {
+                        this.timer2.Enabled = false;
+                        Form2 form = new Form2();
+                        form.Show();
+                    }
+                    situation = 4;
+                    
+                    break;
+                case 4:
+                    timer2.Start();
+                    
+                    timer1.Interval = 1000;
+                    label2.Enabled = false;
+                    contador = 1;
+                    contador = contador - 1;
+                    this.LbContador.Text = Convert.ToInt32(contador).ToString();
+                    if (contador == 0)
+                    {
+                        this.timer2.Enabled = false;
+                        Form2 form = new Form2();
+                        form.Show();
+                    }
+                    situation = 0;
+
+                    break;
+
+            }
+        }
+
+        private void BtnIniciar_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            timer2.Start();
+            if (timer1.Interval > 3000)
+            {
+                btnCancelar.Enabled = false;
+
+            }
+            else
+               if (timer1.Interval < 3000)
+            {
+                btnCancelar.Enabled = true;
+            }
+            //btnCancelar.Enabled = false;
+            //btnCancelar.Enabled = true;
+            label1.Text = "Semaforo";
+        }
+
+        private void BtnCancelar_Click(object sender, EventArgs e)
+        {
+            if (btnCancelar.Text == "Cancelar")
+            {
+                timer1.Enabled = false;
+                btnCancelar.Text = "Reanudar";
+                label1.Text = "Semaforo Detenido";
+            }
+            else
+                if (btnCancelar.Text == "Reanudar")
+            {
+                timer1.Enabled = true;
+                btnCancelar.Text = "Cancelar";
+                label1.Text = "Semaforo Encendido";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Semaphore02.Properties.Resources.semaforo_verde;
+            //btnCancelar.Enabled = false;
+            contador = 50;
+            this.LbContador.Text = contador.ToString();
+            this.timer2.Enabled = true;
+            //timer2.Stop();
+            //label2.Enabled = false;
+
+
+        }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            
+            contador = contador - 1;
+            this.LbContador.Text = Convert.ToInt32(contador).ToString();
+            if (contador == 0)
+            {
+                this.timer2.Enabled = false;
+            }
+        }
+    }
+}
